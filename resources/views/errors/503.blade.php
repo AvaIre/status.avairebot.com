7fb6d78d3b63d2aa60f582586763ad71c94f8ac7
Alexis Tan <alexis@sen-dev.com>
2017-07-23 05:06:29 +0200

    Add 503 maintenance blade view

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,47 @@
+<html>
+    <head>
+        <link href='https://fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+
+        <style>
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                height: 100%;
+                color: #AEAEAE;
+                background: #36393E;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 72px;
+                margin-bottom: 40px;
+            }
+
+            .subtext {
+                font-size: 38px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Be right back.</div>
+                <div class="subtext">We're currently under maintenance.<br>We will be back as soon as Senither can get his coffee.</div>
+            </div>
+        </div>
+    </body>
+</html>
