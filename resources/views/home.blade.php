6d7bde2f3f3616abde9c4509e2b59e4d1d88a132
Alexis Tan <alexis@sen-dev.com>
2017-07-23 17:48:32 +0200

    Add "In development" message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,14 @@
 @extends('app')
 
 @section('content')
-    
+    <div class="container">
+        <div class="row">
+            <div class="alert alert-info">
+                <span class="font-large">
+                    <strong>Hi there!</strong>
+                    <br>This page is currently under development, you can follow the progress on <a href="https://github.com/AvaIre/status.avairebot.com" class="alert-link">GitHub</a>.
+                </span>
+            </div>
+        </div>
+    </div>
 @endsection
