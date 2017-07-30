69e97dc032d587fe0b65df11d55010ef19166ded
Alexis Tan <alexis@sen-dev.com>
2017-07-30 21:34:10 +0200

    Change database migrations table name

--- config/database.php
+++ config/database.php
@@ -91,7 +91,7 @@ return [
     |
     */
 
-    'migrations' => 'migrations',
+    'migrations' => 'status_migrations',
 
     /*
     |--------------------------------------------------------------------------
