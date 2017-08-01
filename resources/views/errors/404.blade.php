0d61e958909dff473c5d381d58febaec661155a8
Alexis Tan <alexis@sen-dev.com>
2017-08-01 12:38:00 +0200

    Add 404 error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,61 @@
+<html>
+    <head>
+        <link href='https://fonts.googleapis.com/css?family=Lato:100,500' rel='stylesheet' type='text/css'>
+
+        <style>
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                height: 100%;
+                color: #AEAEAE;
+                background: #36393E;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 72px;
+                margin-bottom: 40px;
+            }
+
+            .subtext {
+                font-size: 38px;
+            }
+
+            a {
+                color: #AEAEAE;
+                font-weight: 500;
+                text-decoration: none;
+            }
+            a:hover {
+                text-decoration: underline;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">
+                    <strong>404 - Not Found!</strong>
+                </div>
+                <div class="subtext">
+                    <strong>Whoops!</strong> Looks like you took the wrong turn somewhere.<br>
+                    You might be able to find what you're looking for from the <a href="/">home page</a>.
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
